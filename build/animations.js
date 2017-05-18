03b2ad5eb00ab7262781a016a8b5f901f17badeb
Claire Chen <xc2282@tc.columbia.edu>
2017-05-18 17:05:47 -0400

    update

--- build/animations.js
+++ build/animations.js
@@ -20,17 +20,17 @@ function goTo(el) {
      $(".navigation-mobile").hide("slide", { direction: "up" }, 400);
  }
 
- $(window).bind('keydown', function(e){
-    if (e.keyCode == 37) {
-        console.log('left');
-    } else if (e.keyCode == 38) {
-        console.log('up');
-    } else if (e.keyCode == 39) {
-        console.log('right');
-    } else if (e.keyCode == 40) {
-        console.log('down');
-    }
-});
+//  $(window).bind('keydown', function(e){
+//     if (e.keyCode == 37) {
+//         console.log('left');
+//     } else if (e.keyCode == 38) {
+//         console.log('up');
+//     } else if (e.keyCode == 39) {
+//         console.log('right');
+//     } else if (e.keyCode == 40) {
+//         console.log('down');
+//     }
+// });
 
  $(".mobile-menu-trigger").click(function(){
     $(".navigation-mobile").toggle();
